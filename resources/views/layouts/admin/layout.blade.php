b17deb449a39e7527287fadcf57e4aa9c969d384
yanjun dong <dongyanjun0321@gmail.com>
2019-02-21 12:18:59 +0900

    put header in layout

--- resources/views/layouts/admin/layout.blade.php
+++ resources/views/layouts/admin/layout.blade.php
@@ -48,56 +48,55 @@
         </div>
         <div class="main-menu">
         <div class="menu-inner">
-        <nav>
-        <ul class="metismenu" id="menu">
-        <li class="active">
-        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Dashboard</span></a>
-    <ul class="collapse">
-        <li class="active"><a href="index.html">user</a></li>
-    <li><a href="index2.html">post</a></li>
-    </ul>
-    </li>
-    <li>
-    <a href="javascript:void(0)" aria-expanded="true"><i class="ti-book"></i><span>Post
-    </span></a>
-    <ul class="collapse">
-        <li><a href="index.html">Post list</a></li>
-        <li><a href="index3-horizontalmenu.html">New post</a></li>
-        <li><a href="index3-horizontalmenu.html">Post manage</a></li>
-    </ul>
-    </li>
-    <li>
-    <a href="javascript:void(0)" aria-expanded="true"><i class="ti-user"></i><span>User</span></a>
-    <ul class="collapse">
-      <li><a href="barchart.html">User list</a></li>
-      <li><a href="linechart.html">User manage</a></li>
-    </ul>
-    </li>
-    <li>
-    <a href="javascript:void(0)" aria-expanded="true"><i class="ti-comments"></i><span>Comment</span></a>
-    <ul class="collapse">
-        <li><a href="accordion.html">Comment list</a></li>
-        <li><a href="accordion.html">Comment manage</a></li>
-    </ul>
-    </li>
-    <li>
-    <a href="javascript:void(0)" aria-expanded="true"><i class="ti-exchange-vertical"></i><span>Flight</span></a>
-    <ul class="collapse">
-        <li><a href="fontawesome.html">Flight list</a></li>
-        <li><a href="themify.html">New flight</a></li>
-        <li><a href="themify.html">Flight manage</a></li>
-    </ul>
-    </li>
-    <li>
-    <a href="javascript:void(0)" aria-expanded="true"><i class="ti-link"></i><span>Brand</span></a>
-    <ul class="collapse">
-        <li><a href="table-basic.html">Brand list</a></li>
-    <li><a href="table-layout.html">New brand</a></li>
-    <li><a href="datatable.html">Brand manage</a></li>
-    </ul>
-    </li>
-    </ul>
-    </nav>
+          <nav>
+          <ul class="metismenu" id="menu">
+            <li class="active">
+              <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Dashboard</span></a>
+              <ul class="collapse">
+                <li class="active"><a href="index.html">user</a></li>
+                <li><a href="index2.html">post</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-book"></i><span>Post</span></a>
+            <ul class="collapse">
+              <li><a href="index.html">Post list</a></li>
+              <li><a href="new_post_form.blade.html">New post</a></li>
+              <li><a href="index3-horizontalmenu.html">Post manage</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-user"></i><span>User</span></a>
+            <ul class="collapse">
+              <li><a href="barchart.html">User list</a></li>
+              <li><a href="linechart.html">User manage</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-comments"></i><span>Comment</span></a>
+            <ul class="collapse">
+                <li><a href="accordion.html">Comment list</a></li>
+                <li><a href="accordion.html">Comment manage</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-exchange-vertical"></i><span>Flight</span></a>
+            <ul class="collapse">
+                <li><a href="fontawesome.html">Flight list</a></li>
+                <li><a href="themify.html">New flight</a></li>
+                <li><a href="themify.html">Flight manage</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-link"></i><span>Brand</span></a>
+            <ul class="collapse">
+                <li><a href="table-basic.html">Brand list</a></li>
+            <li><a href="table-layout.html">New brand</a></li>
+            <li><a href="datatable.html">Brand manage</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     </div>
     </div>
     </div>
@@ -105,7 +104,173 @@
 
     <!-- main content area start -->
     <div class="main-content">
-
+      <!-- header area start -->
+      <div class="header-area">
+          <div class="row align-items-center">
+              <!-- nav and search button -->
+              <div class="col-md-6 col-sm-8 clearfix">
+                  <div class="nav-btn pull-left">
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                  </div>
+                  <div class="search-box pull-left">
+                      <form action="#">
+                          <input type="text" name="search" placeholder="Search..." required>
+                          <i class="ti-search"></i>
+                      </form>
+                  </div>
+              </div>
+              <!-- profile info & task notification -->
+              <div class="col-md-6 col-sm-4 clearfix">
+                  <ul class="notification-area pull-right">
+                      <li id="full-view"><i class="ti-fullscreen"></i></li>
+                      <li id="full-view-exit"><i class="ti-zoom-out"></i></li>
+                      <li class="dropdown">
+                          <i class="ti-bell dropdown-toggle" data-toggle="dropdown">
+                              <span>2</span>
+                          </i>
+                          <div class="dropdown-menu bell-notify-box notify-box">
+                              <span class="notify-title">You have 3 new notifications <a href="#">view all</a></span>
+                              <div class="nofity-list">
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb"><i class="ti-key btn-danger"></i></div>
+                                      <div class="notify-text">
+                                          <p>You have Changed Your Password</p>
+                                          <span>Just Now</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb"><i class="ti-comments-smiley btn-info"></i></div>
+                                      <div class="notify-text">
+                                          <p>New Commetns On Post</p>
+                                          <span>30 Seconds ago</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb"><i class="ti-key btn-primary"></i></div>
+                                      <div class="notify-text">
+                                          <p>Some special like you</p>
+                                          <span>Just Now</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb"><i class="ti-comments-smiley btn-info"></i></div>
+                                      <div class="notify-text">
+                                          <p>New Commetns On Post</p>
+                                          <span>30 Seconds ago</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb"><i class="ti-key btn-primary"></i></div>
+                                      <div class="notify-text">
+                                          <p>Some special like you</p>
+                                          <span>Just Now</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb"><i class="ti-key btn-danger"></i></div>
+                                      <div class="notify-text">
+                                          <p>You have Changed Your Password</p>
+                                          <span>Just Now</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb"><i class="ti-key btn-danger"></i></div>
+                                      <div class="notify-text">
+                                          <p>You have Changed Your Password</p>
+                                          <span>Just Now</span>
+                                      </div>
+                                  </a>
+                              </div>
+                          </div>
+                      </li>
+                      <li class="dropdown">
+                          <i class="fa fa-envelope-o dropdown-toggle" data-toggle="dropdown"><span>3</span></i>
+                          <div class="dropdown-menu notify-box nt-enveloper-box">
+                              <span class="notify-title">You have 3 new notifications <a href="#">view all</a></span>
+                              <div class="nofity-list">
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb">
+                                          <img src="assets/images/author/author-img1.jpg" alt="image">
+                                      </div>
+                                      <div class="notify-text">
+                                          <p>Aglae Mayer</p>
+                                          <span class="msg">Hey I am waiting for you...</span>
+                                          <span>3:15 PM</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb">
+                                          <img src="assets/images/author/author-img2.jpg" alt="image">
+                                      </div>
+                                      <div class="notify-text">
+                                          <p>Aglae Mayer</p>
+                                          <span class="msg">When you can connect with me...</span>
+                                          <span>3:15 PM</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb">
+                                          <img src="assets/images/author/author-img3.jpg" alt="image">
+                                      </div>
+                                      <div class="notify-text">
+                                          <p>Aglae Mayer</p>
+                                          <span class="msg">I missed you so much...</span>
+                                          <span>3:15 PM</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb">
+                                          <img src="assets/images/author/author-img4.jpg" alt="image">
+                                      </div>
+                                      <div class="notify-text">
+                                          <p>Aglae Mayer</p>
+                                          <span class="msg">Your product is completely Ready...</span>
+                                          <span>3:15 PM</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb">
+                                          <img src="assets/images/author/author-img2.jpg" alt="image">
+                                      </div>
+                                      <div class="notify-text">
+                                          <p>Aglae Mayer</p>
+                                          <span class="msg">Hey I am waiting for you...</span>
+                                          <span>3:15 PM</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb">
+                                          <img src="assets/images/author/author-img1.jpg" alt="image">
+                                      </div>
+                                      <div class="notify-text">
+                                          <p>Aglae Mayer</p>
+                                          <span class="msg">Hey I am waiting for you...</span>
+                                          <span>3:15 PM</span>
+                                      </div>
+                                  </a>
+                                  <a href="#" class="notify-item">
+                                      <div class="notify-thumb">
+                                          <img src="assets/images/author/author-img3.jpg" alt="image">
+                                      </div>
+                                      <div class="notify-text">
+                                          <p>Aglae Mayer</p>
+                                          <span class="msg">Hey I am waiting for you...</span>
+                                          <span>3:15 PM</span>
+                                      </div>
+                                  </a>
+                              </div>
+                          </div>
+                      </li>
+                      <li class="settings-btn">
+                          <i class="ti-settings"></i>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+                  <!-- header area end -->
         @yield("content")
 
     </div>
