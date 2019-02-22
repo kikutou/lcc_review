ae25c791e94559c2d212f8d3cde6a25fe6390af9
unknown <cheng19941029@gmail.com>
2019-02-22 11:26:32 +0900

    valiadtor of country

--- app/Http/Controllers/Admin/CountryController.php
+++ app/Http/Controllers/Admin/CountryController.php
@@ -5,14 +5,22 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\Master\Country;
+use Validator;
 
 class CountryController extends Controller
 {
+
   public function add(Request $request){
 
     if($request->isMethod("GET")) {
       return view("admin.country.add");
     } else {
+
+      $validator = Validator::make($request->all(), Country::$validation_rules, Country::$validation_messages);
+      if($validator->fails()) {
+        return redirect(route("admin_get_country_add"))->withErrors($validator)->withInput();
+      }
+
       $country = new Country;
       $country->value = $request->value;
       $country->rank = $request->rank;
--- app/Model/Master/Country.php
+++ app/Model/Master/Country.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     protected $table = "mtb_countries";
+
+    public static $validation_rules = [
+      "value" => "required|between:1,15",
+      "rank" => "required|integer|unique:mtb_countries, rank"
+    ];
+
+    public static $validation_messages = [
+      "value.required" => "国名を入力してください。",
+      "value.between" => ":min以上:max以内の国名を入力してください。",
+      "rank.required" => "ランクを入力してください。"
+    ];
 }
