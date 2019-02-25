5d6a355fcdecc115325e3c105aeb30daf2b94941
yanjun dong <dongyanjun0321@gmail.com>
2019-02-25 15:33:47 +0900

    change layout head

--- resources/views/layouts/admin/layout.blade.php
+++ resources/views/layouts/admin/layout.blade.php
@@ -402,10 +402,10 @@
           <div class="row align-items-center">
               <div class="col-sm-6">
                   <div class="breadcrumbs-area clearfix">
-                      <h4 class="page-title pull-left">Dashboard</h4>
+                      <h4 class="page-title pull-left">LCC Admin</h4>
                       <ul class="breadcrumbs pull-left">
                           <li><a href="admin">Home</a></li>
-                          <li><span>Form</span></li>
+
                       </ul>
                   </div>
               </div>
