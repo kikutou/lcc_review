004fb6a228998db4574102d387c2e746c5732833
Vermouth-C <cheng19941029@gmail.com>
2019-02-26 12:01:53 +0900

    lcc

--- database/migrations/2019_02_18_082144_create_mtb_airport_table.php
+++ database/migrations/2019_02_18_082144_create_mtb_airport_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMtbAirportStatusTable extends Migration
+class CreateMtbAirportTable extends Migration
 {
     /**
      * Run the migrations.
