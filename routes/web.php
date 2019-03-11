a3e6a00f4a667290baf29a940e9d72fcf1fad2c1
Vermouth-C <cheng19941029@gmail.com>
2019-03-11 11:01:06 +0900

    123456

--- routes/web.php
+++ routes/web.php
@@ -128,3 +128,6 @@ Route::get("user/user/add", "User\UserController@add")->name("user_get_user_add"
 Route::post("user/user/add", "User\UserController@add")->name("user_post_user_add");
 //Home
 Route::get("home", "User\HomeController@index")->name("user_get_home");
+
+//mail test
+Route::get('mail/send','MailController@send');
